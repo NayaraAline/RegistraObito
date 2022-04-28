80bd83a6602a5e9755c13c8096699b0e0b0778d3
NayaraAline <nayara.aline@romaconsulting.com.br>
2022-04-28 10:20:16 -0300

    Teste

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = {
-    path('', views.index, name='index'),
+    path('index.html/', views.index, name='index'),
     path('sobre/', views.sobre, name='sobre'),
     path('pesquisa/', views.pesquisa, name='pesquisa'),
 
